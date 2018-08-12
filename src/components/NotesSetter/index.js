68530606f534a5fdbe60ccb751fe63d6a9dabb6a
Walmik <walmik.deshpande@gmail.com>
2018-08-11 17:08:39 -0700

    Add logo text

--- src/components/NotesSetter/index.js
+++ src/components/NotesSetter/index.js
@@ -17,7 +17,7 @@ const NotesSetter = ({ currentScale, currentChord, notesType, rootNote }) => {
 	const getChordScaleDropdown = () => {
 		var notesTypeData = getScaleNames(), ddType = 'scale';
 		var selectedValue = currentScale;
-		console.log('here now', notesType);
+
 		if (notesType === 'chord') {
 			notesTypeData = getChordNames();
 			ddType = 'chord';
